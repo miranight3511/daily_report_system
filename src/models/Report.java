9253d62e59360affdb13ac0c37230861029da5a4
mira <miranight3511@gmail.com>
2020-11-15 22:00:44 +0900

    AddReport index

--- src/models/Report.java
+++ src/models/Report.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 @Table(name = "reports")
 @NamedQueries({
     @NamedQuery(
-            name = "getAllReportts",
+            name = "getAllReports",
             query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
             ),
     @NamedQuery(
@@ -28,84 +28,84 @@ import javax.persistence.Table;
 })
 @Entity
 public class Report {
-@Id
-@Column(name = "id")
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-@ManyToOne
-@JoinColumn(name = "employee_id",nullable = false)
-private Employee employee;
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
 
-@Column(name = "report_date",nullable = false)
-private Date report_date;
+    @Column(name = "report_date", nullable = false)
+    private Date report_date;
 
-@Column(name = "title",length = 255,nullable = false)
-private String title;
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
-@Lob
-@Column(name = "content",nullable = false)
-private String content;
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
 
-@Column(name = "created_at",nullable = false)
-private Timestamp created_at;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
 
-@Column(name = "updated_at",nullable = false)
-private Timestamp updated_at;
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
-public Integer getId() {
-    return id;
-}
+    public Integer getId() {
+        return id;
+    }
 
-public void setId(Integer id) {
-    this.id = id;
-}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-public Employee getEmployee() {
-    return employee;
-}
+    public Employee getEmployee() {
+        return employee;
+    }
 
-public void setEmployee(Employee employee) {
-    this.employee = employee;
-}
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 
-public Date getReport_date() {
-    return report_date;
-}
+    public Date getReport_date() {
+        return report_date;
+    }
 
-public void setReport_date(Date report_date) {
-    this.report_date = report_date;
-}
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
 
-public String getTitle() {
-    return title;
-}
+    public String getTitle() {
+        return title;
+    }
 
-public void setTitle(String title) {
-    this.title = title;
-}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-public String getContent() {
-    return content;
-}
+    public String getContent() {
+        return content;
+    }
 
-public void setContent(String content) {
-    this.content = content;
-}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-public Timestamp getCreated_at() {
-    return created_at;
-}
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
 
-public void setCreated_at(Timestamp created_at) {
-    this.created_at = created_at;
-}
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
 
-public Timestamp getUpdated_at() {
-    return updated_at;
-}
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
 
-public void setUpdated_at(Timestamp updated_at) {
-    this.updated_at = updated_at;
-}
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 }
